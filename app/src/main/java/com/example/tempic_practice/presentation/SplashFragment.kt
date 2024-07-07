04855031036a040bccafdff331b702f62bbbabae
linhdtn <hallinnh741@gmail.com>
2024-07-08 00:57:56 +0700

    navigate

--- app/src/main/java/com/example/tempic_practice/presentation/SplashFragment.kt
+++ app/src/main/java/com/example/tempic_practice/presentation/SplashFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.tempic_practice.R
 import com.example.tempic_practice.databinding.SplashFragmentBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
     private lateinit var binding: SplashFragmentBinding
@@ -24,6 +29,10 @@ class SplashFragment : Fragment() {
         val params = ivSplash.layoutParams
         params.width = (widthScreen * 0.58).toInt()
         ivSplash.layoutParams = params
+        lifecycleScope.launch {
+            delay(3000)
+            findNavController().navigate(R.id.homeFragment)
+        }
         super.onViewCreated(view, savedInstanceState)
     }
 }
